cd903432930be735ec5b48b8fba659a8d04870d6
Silmarill <mrsilmar@gmail.com>
2026-02-06 14:16:50 +0700

    New files

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0)
+            Console.WriteLine($"{number} - четное число.");
+        else
+            Console.WriteLine($"{number} - нечётное число.");
+    }
+}
