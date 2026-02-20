c583b99aeaa2ffce2774fd536e757146d13d7839
Silmarill <mrsilmar@gmail.com>
2026-02-20 12:44:01 +0700

    Ещё правки

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ class Program
         Console.Write("Введите число: ");
         number = Convert.ToInt32(Console.ReadLine());
 
-        if (number % 29 == 0) {
+        if (number % 2559 == 0) {
             Console.WriteLine($"{number} - четное число.");
         } else {
             Console.WriteLine($"{number} - нечётное число.");
