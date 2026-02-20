043c3b07e62835cd8a587aa53020e083c76adced
Silmarill <mrsilmar@gmail.com>
2026-02-20 11:54:43 +0700

    test new PC

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ class Program
         Console.Write("Введите число: ");
         number = Convert.ToInt32(Console.ReadLine());
 
-        if (number % 9 == 0) {
+        if (number % 1229 == 0) {
             Console.WriteLine($"{number} - четное число.");
         } else {
             Console.WriteLine($"{number} - нечётное число.");
